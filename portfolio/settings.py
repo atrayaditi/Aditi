275fa4615c368827221d0a45e09ae80f2c4a2296
Adity atray <atrayaditi015@gmail.com>
2025-06-27 19:03:11 +0530

    commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -128,4 +128,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 import os
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'staticfiles_build', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static_build', 'static')]
